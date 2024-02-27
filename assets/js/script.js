44024102f6aa6c55ecb647c91257fe5e62451e98
JonathanBC2 <147190800+JonathanBC2@users.noreply.github.com>
2024-02-27 16:15:12 -0500

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
