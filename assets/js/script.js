5b3939bca7e72ce3418163f2eabbeaade333200b
JonathanBC2 <147190800+JonathanBC2@users.noreply.github.com>
2024-02-29 08:51:46 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@
  * loading will be end after document is loaded
  */
 
-const preloader = document.querySelector("[data-preaload]");
+// const preloader = document.querySelector("[data-preaload]");
 
-window.addEventListener("load", function () {
-    preloader.classList.add("loaded");
-    document.body.classList.add("loaded");
-});
+// window.addEventListener("load", function () {
+//     preloader.classList.add("loaded");
+//     document.body.classList.add("loaded");
+// });
